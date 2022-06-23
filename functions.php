c9868b44773a35a0c9c56f1cf0cddf7e6070e325
mateuswetah <mateus.m.luna@gmail.com>
2022-06-23 15:03:39 -0300

    Version 0.1.6.

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 Theme Name: Midiateca
 Description: Midiateca Capixaba - a child theme of Blocksy
 Author: wetah
-Version: 0.1.5
+Version: 0.1.6
 Text Domain: midiateca
 */
 
@@ -12,7 +12,7 @@ if (! defined('WP_DEBUG')) {
 }
 
 /** Child Theme version */
-const MIDIATECA_VERSION = '0.1.5';
+const MIDIATECA_VERSION = '0.1.6';
 
 /* Enqueues necessary JS and CSS files */
 add_action( 'wp_enqueue_scripts', function () {
