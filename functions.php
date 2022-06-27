0547a1380cf116c633c55940b4f505705d0b8ab9
mateuswetah <mateus.m.luna@gmail.com>
2022-06-27 11:00:15 -0300

    Adjust facets grid term.

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 Theme Name: Midiateca
 Description: Midiateca Capixaba - a child theme of Blocksy
 Author: wetah
-Version: 0.1.6
+Version: 0.1.7
 Text Domain: midiateca
 */
 
@@ -12,7 +12,7 @@ if (! defined('WP_DEBUG')) {
 }
 
 /** Child Theme version */
-const MIDIATECA_VERSION = '0.1.6';
+const MIDIATECA_VERSION = '0.1.7';
 
 /* Enqueues necessary JS and CSS files */
 add_action( 'wp_enqueue_scripts', function () {
